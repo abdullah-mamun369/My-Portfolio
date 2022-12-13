5b16a12e5bd362a188c86840c0514b6b7a1221d9
abdullah-mamun369 <maamamun369@gmail.com>
2022-12-13 13:25:25 +0600

    About header design is completed

--- src/Components/Home/Home Children/About.js
+++ src/Components/Home/Home Children/About.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import '../Home.css'
 
 const About = () => {
     return (
         <div className='w-screen h-screen bg-[#222]'>
-            <div className='container mx-auto'>
+            <div className='container mx-auto grid'>
                 <h1 className='text-6xl font-black text-center pt-20 text-white uppercase'>About <span className='text-primary'>Me</span></h1>
-                <p className='text-center text-base-300 mt-7 before:divide-x before:divide-white before:divide-solid'>I DESIGN AND CODE BEAUTIFUL THINGS, AND I LOVE WHAT I DO.</p>
+                <div className='relative m-auto'>
+                    <p className='text-base-300 mt-7 paragraph-design title-paragraph'>I DESIGN AND CODE BEAUTIFUL THINGS, AND I LOVE WHAT I DO.</p>
+                </div>
+
             </div>
         </div>
     );
